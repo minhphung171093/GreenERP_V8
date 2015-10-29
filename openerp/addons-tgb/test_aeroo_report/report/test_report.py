f6c94ae87bf5651b492e73815d695e0db53551e6
Phung Pham <minhphung171093@gmail.com>
2015-10-30 06:43:59 +0700

    them aeroo

---
+++ openerp/addons-tgb/test_aeroo_report/report/test_report.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#
+##############################################################################
+
+import time
+from openerp.report import report_sxw
+from openerp.osv import osv
+from openerp.tools.translate import _
+import random
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+DATE_FORMAT = "%Y-%m-%d"
+
+class Parser(report_sxw.rml_parse):
+        
+    def __init__(self, cr, uid, name, context):
+        super(Parser, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'get_datenow': self.get_datenow,
+        })
+        
+    def get_datenow(self):
+        return time.strftime('%Y-%m-%d')
+    
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
