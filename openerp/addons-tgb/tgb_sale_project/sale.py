0a58d371880c2f3b666cc9e4096478d0f89a5f0a
Phung Pham <minhphung171093@gmail.com>
2015-11-25 11:14:40 +0700

    chinh sua report

--- openerp/addons-tgb/tgb_sale_project/sale.py
+++ openerp/addons-tgb/tgb_sale_project/sale.py
@@ -35,7 +35,15 @@ import calendar
 
 class sale_order(osv.osv):
     _inherit = 'sale.order'
-
+    
+    def print_quotation(self, cr, uid, ids, context=None):
+        '''
+        This function prints the sales order and mark it as sent, so that we can see more easily the next step of the workflow
+        '''
+        assert len(ids) == 1, 'This option should only be used for a single id at a time'
+        self.signal_workflow(cr, uid, ids, 'quotation_sent')
+        return self.pool['report'].get_action(cr, uid, ids, 'tgb_sale_project.report_sample_with_one_project', context=context)
+    
     _columns = {
         'list_project_line': fields.one2many('list.project','order_id','List Project'),
     }
