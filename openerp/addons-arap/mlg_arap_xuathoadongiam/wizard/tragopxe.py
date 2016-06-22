bdeee5f6f8512087a54df97992f7d0f8292402d4
Phung Pham <minhphung171093@gmail.com>
2016-06-22 22:18:21 +0700

    up

--- openerp/addons-arap/mlg_arap_xuathoadongiam/wizard/tragopxe.py
+++ openerp/addons-arap/mlg_arap_xuathoadongiam/wizard/tragopxe.py
@@ -26,7 +26,7 @@ class tragopxe_wizard(osv.osv_memory):
         this = self.browse(cr, uid, ids[0])
         user = self.pool.get('res.users').browse(cr, uid, uid)
         sql = '''
-            select id from account_invoice where mlg_type='tra_gop_xe' and bien_so_xe_id=%s and chinhanh_id=%s
+            select id from account_invoice where mlg_type='tra_gop_xe' and bien_so_xe_id=%s and chinhanh_id=%s and state!='cancel'
                 and thu_cho_doituong_id in (select id from res_partner where ma_doi_tuong='%s' and nhadautugiantiep=True)
         '''%(this.bien_so_xe_id.id, user.chinhanh_id.id,user.chinhanh_id.code)
         cr.execute(sql)
